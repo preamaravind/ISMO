3e704c045b503f46208ac580a2d8c979ed7c0fed
preamaravind <preamkumararavind6@gmail.com>
2026-06-23 13:03:50 +0530

    Replace database authentication with sync method for serverless environment

--- ProjectFlow/api/index.js
+++ ProjectFlow/api/index.js
@@ -1,11 +1,11 @@
 const app = require('../server/app');
 const { sequelize } = require('../server/models');
 
-// Authenticate database (Sequelize handles connection pooling)
-// For serverless, we avoid doing heavy schema syncs on every request
-sequelize.authenticate()
-  .then(() => console.log('Database connected successfully in serverless environment.'))
-  .catch(err => console.error('Unable to connect to the database:', err));
+// Sync database to ensure tables are created in the Neon Postgres DB
+// CREATE TABLE IF NOT EXISTS is safe to run in serverless
+sequelize.sync()
+  .then(() => console.log('Database synced successfully in serverless environment.'))
+  .catch(err => console.error('Unable to sync the database:', err));
 
 // Export the Express app as a serverless function handler
 module.exports = app;
